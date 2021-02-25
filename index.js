5c039b158a9db1dc9c13fa9edfd12f056394d170
Sayantan Mishra <sayanmdn@gmail.com>
2021-02-25 11:27:05 +0530

    ssl add

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const https = require('https')
 const authRouter = require('./routes/auth')
 const postRoute = require('./routes/private') 
 const dotenv = require('dotenv')
+const path = require('path')
 dotenv.config()
 const db = require('./db')
+const fs = require('fs')
 
 const app = express()
 app.use(cors())
@@ -21,5 +24,17 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
+})
+
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+  },
+  app
+)
+
+sslServer.listen(3443, ()=>{
+  console.log('App listening at https://localhost:3443')
 })
